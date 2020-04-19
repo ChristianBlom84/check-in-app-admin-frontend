2194d87a992575dcb3264e4e82a64d9ead26271f
Christian Blom <christian.blom@chasacademy.se>
2020-04-19 15:52:26 +0200

    Added CRUD to subscribers.

--- src/components/pages/Subscribers.tsx
+++ src/components/pages/Subscribers.tsx
@@ -1,11 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import SubscriberList from '../partials/SubscriberList';
+import SubscriberModal from '../partials/SubscriberModal';
 import Spinner from '../partials/Spinner';
 import styles from './Subscribers.module.scss';
 
+export interface Subscriber {
+  _id?: string;
+  name: string;
+  email: string;
+  notifications?: Notification[];
+  organization?: string;
+  pushToken: string;
+}
+
 const Subscribers: React.FC = () => {
-  const [subscribers, setSubscribers] = useState([]);
+  const [subscribers, setSubscribers] = useState<Subscriber[]>();
+  const [editing, setEditing] = useState(false);
+  const [editInfo, setEditInfo] = useState<Subscriber>();
 
   const fetchSubscribers = async (): Promise<void> => {
     try {
@@ -24,10 +36,23 @@ const Subscribers: React.FC = () => {
 
   return subscribers ? (
     <main className={styles.main}>
+      {editing ? (
+        <SubscriberModal
+          editInfo={editInfo}
+          setEditing={setEditing}
+          setEditInfo={setEditInfo}
+          subscribers={subscribers}
+          setSubscribers={setSubscribers}
+        />
+      ) : null}
       <div className={styles.userList}>
         <h2>Organization Name</h2>
         <h3>Subscribers:</h3>
-        <SubscriberList subscribers={subscribers} />
+        <SubscriberList
+          setEditing={setEditing}
+          setEditInfo={setEditInfo}
+          subscribers={subscribers}
+        />
       </div>
     </main>
   ) : (
--- src/components/partials/SubscriberList.tsx
+++ src/components/partials/SubscriberList.tsx
@@ -1,32 +1,50 @@
 import React, { Fragment } from 'react';
+import { Subscriber } from '../pages/Subscribers';
 import styles from './SubscriberList.module.scss';
-
-interface Subscriber {
-  _id: string;
-  name: string;
-  email: string;
-  pushToken: number;
-}
+import EditIcon from '@material-ui/icons/Edit';
 
 interface Props {
   subscribers: Subscriber[];
+  setEditing: (editing: boolean) => void;
+  setEditInfo: (subscriber: Subscriber) => void;
 }
 
-const SubscriberList: React.FC<Props> = ({ subscribers }: Props) => {
+const SubscriberList: React.FC<Props> = ({
+  subscribers,
+  setEditing,
+  setEditInfo
+}: Props) => {
+  const openModal = (subscriber: Subscriber): void => {
+    setEditInfo({
+      name: subscriber.name,
+      email: subscriber.email,
+      organization: subscriber.organization,
+      pushToken: subscriber.pushToken
+    });
+    setEditing(true);
+  };
+
   return subscribers ? (
     <ul className={styles.list}>
       {subscribers.map(subscriber => (
-        <li key={subscriber._id}>
-          <p>
-            Name: <span className={styles.subInfo}>{subscriber.name}</span>
-          </p>
-          <p>
-            Email: <span className={styles.subInfo}>{subscriber.email}</span>
-          </p>
-          <p>
-            Push Token:{' '}
-            <span className={styles.subInfo}>{subscriber.pushToken}</span>
-          </p>
+        <li className={styles.subscriber} key={subscriber._id}>
+          <div>
+            <h4>{subscriber.name}</h4>
+            <p>
+              Email: <span className={styles.subInfo}>{subscriber.email}</span>
+            </p>
+            <p>
+              Organization:{' '}
+              <span className={styles.subInfo}>{subscriber.organization}</span>
+            </p>
+            <p>
+              Push Token:{' '}
+              <span className={styles.subInfo}>{subscriber.pushToken}</span>
+            </p>
+          </div>
+          <button onClick={(): void => openModal(subscriber)}>
+            <EditIcon />
+          </button>
         </li>
       ))}
     </ul>
